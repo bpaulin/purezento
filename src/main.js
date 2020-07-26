e4e546fe835e576ed8a1ff18fdd7bf0ce42a97da
bpaulin <brunopaulin@bpaulin.net>
2020-07-26 12:05:23 +0000

    vuefire

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import { firestorePlugin } from "vuefire";
 import router from "./router";
 
 Vue.config.productionTip = false;
+Vue.use(firestorePlugin);
 
 new Vue({
   vuetify,
