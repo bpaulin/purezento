a9b9106784f97821a534e8daf51b690944506473
bpaulin <brunopaulin@bpaulin.net>
2020-07-26 19:34:57 +0000

    projects

--- src/router.js
+++ src/router.js
@@ -19,6 +19,12 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "@/views/About.vue"),
   },
+  {
+    path: "/projects",
+    name: "Projects",
+    component: () =>
+      import(/* webpackChunkName: "projects" */ "@/views/Projects.vue"),
+  },
 ];
 
 const router = new VueRouter({
